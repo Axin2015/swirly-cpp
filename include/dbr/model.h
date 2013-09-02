6ebc91c3b27a8a1a0d7035df07f5a357c1e01199
Mark Aylett <mark.aylett@gmail.com>
2013-09-02 06:40:44 +0100

    Add c++ headers to doxygen.

--- include/dbr/model.h
+++ include/dbr/model.h
@@ -27,14 +27,15 @@
  */
 
 struct DbrModelVtbl {
-    /**
-     * Read entity list where end is assumed to be null.
-     *
-     * The caller is responsible for freeing the resulting list.
-     */
     ssize_t
     (*read_entity)(DbrModel model, int type, DbrPool pool, struct DbrSlNode** first);
 };
+
+/**
+ * @brief Read entity list for type.
+ *
+ * The caller is responsible for freeing the resulting list.
+ */
 
 static inline ssize_t
 dbr_model_read_entity(DbrModel model, int type, DbrPool pool, struct DbrSlNode** first)
