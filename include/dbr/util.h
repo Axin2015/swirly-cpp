56397e2c0dd33e4972aa7f5cec34d608a7460973
Mark Aylett <mark.aylett@gmail.com>
2014-03-13 21:20:42 +0000

    Added trim functions.

--- include/dbr/util.h
+++ include/dbr/util.h
@@ -20,6 +20,7 @@
 
 #include <dbr/defs.h>
 
+#include <ctype.h>  // isspace()
 #include <stddef.h> // size_t
 
 /**
@@ -43,6 +44,24 @@ static inline size_t
 dbr_longlen(long l)
 {
     return dbr_longdig(l) + (l < 0 ? 1 : 0);
+}
+
+static inline const char*
+dbr_ltrim(const char* begin, const char* end)
+{
+    // Skip leading whitespace.
+    while (begin != end && isspace(*begin))
+        ++begin;
+    return begin;
+}
+
+static inline const char*
+dbr_rtrim(const char* begin, const char* end)
+{
+    // Skip trailing whitespace.
+    while (begin != end && isspace(*(end - 1)))
+        --end;
+    return end;
 }
 
 /**
