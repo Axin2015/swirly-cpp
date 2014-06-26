62c6ac544ef56410a7cfa8f2e737b3b98fd78bf1
Mark Aylett <mark.aylett@gmail.com>
2014-06-26 06:26:04 +0100

    Adopted Nginx naming convention.

--- src/ngx/ngx_http_doobry_module.c
+++ src/ngx/ngx_http_doobry_module.c
@@ -20,25 +20,26 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-#include <dbr/elm/types.h>
+#include <dbr/fig.h>
 
-struct Conf {
+typedef struct {
     DbrBool doobry;
     ngx_str_t mdaddr;
     ngx_str_t traddr;
     ngx_uint_t tmout;
     ngx_uint_t capacity;
-};
+    DbrCtx ctx;
+} ngx_http_doobry_loc_conf_t;
 
 static void
-cleanup_loc_conf(void* data)
+ngx_http_doobry_cleanup(void* data)
 {
 }
 
 static void*
-create_loc_conf(ngx_conf_t* cf)
+ngx_http_doobry_create_loc_conf(ngx_conf_t* cf)
 {
-    struct Conf* lcf = ngx_pcalloc(cf->pool, sizeof(struct Conf));
+    ngx_http_doobry_loc_conf_t* lcf = ngx_pcalloc(cf->pool, sizeof(ngx_http_doobry_loc_conf_t));
     if (!lcf) {
         ngx_log_error(NGX_LOG_ERR, cf->log, 0, "failed to allocate local config");
         return NULL;
@@ -53,16 +54,16 @@ create_loc_conf(ngx_conf_t* cf)
         return NULL;
     }
 
-    cln->handler = cleanup_loc_conf;
+    cln->handler = ngx_http_doobry_cleanup;
     cln->data = lcf;
     return lcf;
 }
 
 static char*
-merge_loc_conf(ngx_conf_t* cf, void* prev, void* conf)
+ngx_http_doobry_merge_loc_conf(ngx_conf_t* cf, void* prev, void* conf)
 {
-    struct Conf* parent = prev;
-    struct Conf* child = conf;
+    ngx_http_doobry_loc_conf_t* parent = prev;
+    ngx_http_doobry_loc_conf_t* child = conf;
 
     ngx_conf_merge_str_value(child->mdaddr, parent->mdaddr, "tcp://localhost:3270");
     ngx_conf_merge_str_value(child->traddr, parent->traddr, "tcp://localhost:3271");
@@ -73,7 +74,7 @@ merge_loc_conf(ngx_conf_t* cf, void* prev, void* conf)
 }
 
 static ngx_int_t
-http_handler(ngx_http_request_t* r)
+ngx_http_doobry_handler(ngx_http_request_t* r)
 {
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
     r->headers_out.content_type.data = (u_char*)"text/plain";
@@ -99,88 +100,97 @@ http_handler(ngx_http_request_t* r)
 }
 
 static char*
-set_doobry(ngx_conf_t* cf, ngx_command_t* cmd, void* conf)
+ngx_http_doobry(ngx_conf_t* cf, ngx_command_t* cmd, void* conf)
 {
     ngx_http_core_loc_conf_t* clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     // Handler for the 'doobry' directive.
-    clcf->handler = http_handler;
+    clcf->handler = ngx_http_doobry_handler;
+
+    ngx_http_doobry_loc_conf_t* lcf = conf;
+    lcf->doobry = DBR_TRUE;
+
     return NGX_CONF_OK;
 }
 
 static char*
-set_doobry_mdaddr(ngx_conf_t* cf, void* post, void* data)
+ngx_http_doobry_mdaddr(ngx_conf_t* cf, void* post, void* data)
 {
     return NGX_CONF_OK;
 }
 
 static char*
-set_doobry_traddr(ngx_conf_t* cf, void* post, void* data)
+ngx_http_doobry_traddr(ngx_conf_t* cf, void* post, void* data)
 {
     return NGX_CONF_OK;
 }
 
 static char*
-set_doobry_tmout(ngx_conf_t* cf, void* post, void* data)
+ngx_http_doobry_tmout(ngx_conf_t* cf, void* post, void* data)
 {
     return NGX_CONF_OK;
 }
 
 static char*
-set_doobry_capacity(ngx_conf_t* cf, void* post, void* data)
+ngx_http_doobry_capacity(ngx_conf_t* cf, void* post, void* data)
 {
     return NGX_CONF_OK;
 }
+
+static ngx_conf_post_handler_pt ngx_http_doobry_mdaddr_p = ngx_http_doobry_mdaddr;
+static ngx_conf_post_handler_pt ngx_http_doobry_traddr_p = ngx_http_doobry_traddr;
+static ngx_conf_post_handler_pt ngx_http_doobry_tmout_p = ngx_http_doobry_tmout;
+static ngx_conf_post_handler_pt ngx_http_doobry_capacity_p = ngx_http_doobry_capacity;
 
 // Never called?
 static ngx_int_t
-init_master(ngx_log_t* log)
+ngx_http_doobry_init_master(ngx_log_t* log)
 {
     ngx_log_error(NGX_LOG_NOTICE, log, 0, "init_master()");
     return NGX_OK;
 }
 
 static ngx_int_t
-init_module(ngx_cycle_t* cycle)
+ngx_http_doobry_init_module(ngx_cycle_t* cycle)
 {
     ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "init_module()");
     return NGX_OK;
 }
 
 static ngx_int_t
-init_process(ngx_cycle_t* cycle)
+ngx_http_doobry_init_process(ngx_cycle_t* cycle)
 {
     ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "init_process()");
     return NGX_OK;
 }
 
 static ngx_int_t
-init_thread(ngx_cycle_t* cycle)
+ngx_http_doobry_init_thread(ngx_cycle_t* cycle)
 {
     ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "term_thread()");
     return NGX_OK;
 }
 
 static void
-exit_thread(ngx_cycle_t* cycle)
+ngx_http_doobry_exit_thread(ngx_cycle_t* cycle)
 {
     ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "exit_thread()");
 }
 
 static void
-exit_process(ngx_cycle_t* cycle)
+ngx_http_doobry_exit_process(ngx_cycle_t* cycle)
 {
     ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "exit_process()");
 }
 
 static void
-exit_master(ngx_cycle_t* cycle)
+ngx_http_doobry_exit_master(ngx_cycle_t* cycle)
 {
     ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "exit_master()");
 }
 
 // The context type required for http modules.
 
-static ngx_http_module_t module_ctx = {
+static ngx_http_module_t ngx_http_doobry_module_ctx = {
     // ngx_int_t (*preconfiguration)(ngx_conf_t* cf);
     .preconfiguration = NULL,
     // ngx_int_t (*postconfiguration)(ngx_conf_t* cf);
@@ -194,21 +204,16 @@ static ngx_http_module_t module_ctx = {
     // char* (*merge_srv_conf)(ngx_conf_t* cf, void* prev, void* conf);
     .merge_srv_conf = NULL,
     // void* (*create_loc_conf)(ngx_conf_t* cf);
-    .create_loc_conf = create_loc_conf,
+    .create_loc_conf = ngx_http_doobry_create_loc_conf,
     // char* (*merge_loc_conf)(ngx_conf_t* cf, void* prev, void* conf);
-    .merge_loc_conf = merge_loc_conf
+    .merge_loc_conf = ngx_http_doobry_merge_loc_conf
 };
 
-static ngx_conf_post_handler_pt set_doobry_mdaddr_p = set_doobry_mdaddr;
-static ngx_conf_post_handler_pt set_doobry_traddr_p = set_doobry_traddr;
-static ngx_conf_post_handler_pt set_doobry_tmout_p = set_doobry_tmout;
-static ngx_conf_post_handler_pt set_doobry_capacity_p = set_doobry_capacity;
-
-static ngx_command_t module_commands[] = {
+static ngx_command_t ngx_http_doobry_commands[] = {
     {
         .name = ngx_string("doobry"),
         .type = NGX_HTTP_LOC_CONF | NGX_CONF_NOARGS,
-        .set = set_doobry,
+        .set = ngx_http_doobry,
         .conf = 0,
         .offset = 0,
         .post = NULL
@@ -218,32 +223,32 @@ static ngx_command_t module_commands[] = {
         .type = NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
         .set = ngx_conf_set_str_slot,
         .conf = NGX_HTTP_LOC_CONF_OFFSET,
-        .offset = offsetof(struct Conf, mdaddr),
-        .post = &set_doobry_mdaddr_p
+        .offset = offsetof(ngx_http_doobry_loc_conf_t, mdaddr),
+        .post = &ngx_http_doobry_mdaddr_p
     },
     {
         .name = ngx_string("doobry_traddr"),
         .type = NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
         .set = ngx_conf_set_str_slot,
         .conf = NGX_HTTP_LOC_CONF_OFFSET,
-        .offset = offsetof(struct Conf, traddr),
-        .post = &set_doobry_traddr_p
+        .offset = offsetof(ngx_http_doobry_loc_conf_t, traddr),
+        .post = &ngx_http_doobry_traddr_p
     },
     {
         .name = ngx_string("doobry_tmout"),
         .type = NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
         .set = ngx_conf_set_num_slot,
         .conf = NGX_HTTP_LOC_CONF_OFFSET,
-        .offset = offsetof(struct Conf, tmout),
-        .post = &set_doobry_tmout_p
+        .offset = offsetof(ngx_http_doobry_loc_conf_t, tmout),
+        .post = &ngx_http_doobry_tmout_p
     },
     {
         .name = ngx_string("doobry_capacity"),
         .type = NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
         .set = ngx_conf_set_num_slot,
         .conf = NGX_HTTP_LOC_CONF_OFFSET,
-        .offset = offsetof(struct Conf, capacity),
-        .post = &set_doobry_capacity_p
+        .offset = offsetof(ngx_http_doobry_loc_conf_t, capacity),
+        .post = &ngx_http_doobry_capacity_p
     },
     ngx_null_command
 };
@@ -254,33 +259,33 @@ ngx_module_t ngx_http_doobry_module = {
     // ctx - points to a global context of the module (the actual type of structure this pointer
     // points to depends on the value of type field).
     // void* ctx;
-    .ctx = &module_ctx,
+    .ctx = &ngx_http_doobry_module_ctx,
     // commands - points to a vector of descriptors of module commands.
     // ngx_command_t* commands;
-    .commands = module_commands,
+    .commands = ngx_http_doobry_commands,
     // type - contains the type of the module: NGX_HTTP_MODULE, NGX_EVENT_MODULE, NGX_MAIL_MODULE
     // and others.
     // ngx_uint_t type;
     .type = NGX_HTTP_MODULE,
     // ngx_int_t (*init_master)(ngx_log_t* log);
-    .init_master = init_master,
+    .init_master = ngx_http_doobry_init_master,
     // init_module - points to a handler, that is called upon initialisation of the module in the
     // main process before entering the event loop.
     // ngx_int_t (*init_module)(ngx_cycle_t* cycle);
-    .init_module = init_module,
+    .init_module = ngx_http_doobry_init_module,
     // init_process - points to a handler, that is called upon initialisation of the module in a
     // worker process.
     // ngx_int_t (*init_process)(ngx_cycle_t* cycle);
-    .init_process = init_process,
+    .init_process = ngx_http_doobry_init_process,
     // ngx_int_t (*init_thread)(ngx_cycle_t* cycle);
-    .init_thread = init_thread,
+    .init_thread = ngx_http_doobry_init_thread,
     // void (*exit_thread)(ngx_cycle_t* cycle);
-    .exit_thread = exit_thread,
+    .exit_thread = ngx_http_doobry_exit_thread,
     // exit_process - points to a handler, that is called upon termination of a worker process.
     // void (*exit_process)(ngx_cycle_t* cycle);
-    .exit_process = exit_process,
+    .exit_process = ngx_http_doobry_exit_process,
     // exit_master - points to a handler, that is called upon termination of the master process.
     // void (*exit_master)(ngx_cycle_t* cycle);
-    .exit_master = exit_master,
+    .exit_master = ngx_http_doobry_exit_master,
     NGX_MODULE_V1_PADDING
 };
