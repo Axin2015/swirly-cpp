bb233adaf2051fa417445d45a07f8a7d2c2ce921
Mark Aylett <mark.aylett@gmail.com>
2015-12-26 14:07:10 +0000

    Add stub TraderSess class.

---
+++ include/swirly/fig/TraderSess.hpp
@@ -0,0 +1,51 @@
+/*
+ * Swirly Order-Book and Matching-Engine.
+ * Copyright (C) 2013, 2015 Swirly Cloud Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#ifndef SWIRLY_ELM_TRADERSESS_HPP
+#define SWIRLY_ELM_TRADERSESS_HPP
+
+#include <swirly/elm/Trader.hpp>
+
+namespace swirly {
+
+/**
+ * @addtogroup Entity
+ * @{
+ */
+
+class SWIRLY_API TraderSess : public Trader {
+public:
+    TraderSess(const char* mnem, const char* display, const char* email) noexcept
+    :   Trader{mnem, display, email}
+    {
+    }
+
+    ~TraderSess() noexcept override;
+
+    // Copy.
+    TraderSess(const TraderSess&) = default;
+    TraderSess& operator =(const TraderSess&) = default;
+
+    // Move.
+    TraderSess(TraderSess&&) = default;
+    TraderSess& operator =(TraderSess&&) = default;
+};
+
+/** @} */
+
+} // swirly
+
+#endif // SWIRLY_ELM_TRADERSESS_HPP
