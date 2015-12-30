b06b8585c3fd947bbc0f44e4d5ca90902bbcc1d7
Mark Aylett <mark.aylett@gmail.com>
2015-12-30 18:50:36 +0000

    Add stub MarketBook class.

---
+++ include/swirly/elm/MarketBook.hpp
@@ -0,0 +1,49 @@
+/*
+ * Swirly Order-Book and Matching-Engine.
+ * Copyright (C) 2013, 2015 Swirly Cloud Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#ifndef SWIRLY_ELM_MARKETBOOK_HPP
+#define SWIRLY_ELM_MARKETBOOK_HPP
+
+#include <swirly/elm/Market.hpp>
+
+namespace swirly {
+
+/**
+ * @addtogroup Book
+ * @{
+ */
+
+class SWIRLY_API MarketBook : public Market {
+public:
+    MarketBook(const StringView& mnem, const StringView& display, const StringView& contr,
+               Jd settlDay, Jd expiryDay, MarketState state) noexcept;
+
+    ~MarketBook() noexcept override;
+
+    // Copy.
+    MarketBook(const MarketBook&) = default;
+    MarketBook& operator =(const MarketBook&) = default;
+
+    // Move.
+    MarketBook(MarketBook&&) = default;
+    MarketBook& operator =(MarketBook&&) = default;
+};
+
+/** @} */
+
+} // swirly
+
+#endif // SWIRLY_ELM_MARKETBOOK_HPP
