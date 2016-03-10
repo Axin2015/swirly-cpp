e286d2326a7e1c7edc432328f28fcda411ce944f
Mark Aylett <mark.aylett@gmail.com>
2016-03-10 07:09:23 +0000

    Migrate ftest to swift.

--- test/swift/swift.py
+++ test/swift/swift.py
@@ -1,16 +1,28 @@
-#!/usr/bin/env python
-
-# This script is intended as the basis for automated functional testing.
+# The Restful Matching-Engine.
+# Copyright (C) 2013, 2016 Swirly Cloud Limited.
+#
+# This program is free software; you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; if
+# not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 from contextlib import closing
+
 import httplib
+
 import json
 import os
 import socket
 import subprocess
 import tempfile
 import time
-import unittest
 
 class Process(object):
 
@@ -42,8 +54,7 @@ def getPort():
     return port
 
 def getProg():
-  buildDir = os.getenv('SWIRLY_BUILD')
-  return buildDir + '/bin/swirlyd' if buildDir else 'swirlyd'
+  return os.getenv('SWIRLYD_EXECUTABLE', 'swirlyd')
 
 def readLine(file):
   while True:
@@ -123,32 +134,3 @@ class Connection(object):
     conn.endheaders()
     conn.send(content)
     return Response(conn.getresponse())
-
-class TestSwirly(unittest.TestCase):
-
-  @classmethod
-  def setUpClass(cls):
-    cls.fixture = Fixture()
-
-  @classmethod
-  def tearDownClass(cls):
-    cls.fixture.close()
-    cls = None
-
-  def test_rec(self):
-    with Connection() as conn:
-      self.assertEqual(conn.send('POST', '/api/rec/market').status, 200)
-      self.assertEqual(conn.send('PUT', '/api/rec/market/EURUSD').status, 200)
-      self.assertEqual(conn.send('POST', '/api/rec/trader').status, 200)
-      self.assertEqual(conn.send('PUT', '/api/rec/trader/MARAYL').status, 200)
-
-  def test_sess(self):
-    with Connection() as conn:
-      self.assertEqual(conn.send('POST', '/api/sess/order/EURUSD').status, 200)
-      self.assertEqual(conn.send('PUT', '/api/sess/order/EURUSD/1,2,3').status, 200)
-      self.assertEqual(conn.send('DELETE', '/api/sess/order/EURUSD/1,2,3').status, 200)
-      self.assertEqual(conn.send('POST', '/api/sess/trade/EURUSD').status, 200)
-      self.assertEqual(conn.send('DELETE', '/api/sess/trade/EURUSD/1,2,3').status, 200)
-
-if __name__ == '__main__':
-  unittest.main()
