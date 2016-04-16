54739dc64ccac2e984328d986f13034a319fae33
Mark Aylett <mark.aylett@gmail.com>
2016-04-16 21:23:28 +0100

    Remove format and tidy version.

--- tidy.sh
+++ tidy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 build=${SWIRLY_BUILD:-$HOME/build/swirlyc}
-tidy=${CLANG_FORMAT:-clang-tidy-3.8}
+tidy=${CLANG_FORMAT:-clang-tidy}
 for f in $(find src test -name "*.cpp"); do
   $tidy -p=$build $f
   #$tidy --checks="-*,readability-braces-around-statements" --fix -p=$build $f
