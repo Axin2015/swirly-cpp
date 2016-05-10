85b25f22ea22a4a00dfda4c26afc980dbda928a7
Mark Aylett <mark.aylett@gmail.com>
2016-05-10 06:38:51 +0100

    Exclude RestRequest from format.

--- format.sh
+++ format.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 format=${CLANG_FORMAT:-clang-format}
-find include src test -name "*.[ch]pp" -exec $format -i --style=file {} \;
+find example include src test \( -name "*.[ch]pp" -a ! -name "RestRequest.cpp" \) \
+     -exec $format -i --style=file {} \;
