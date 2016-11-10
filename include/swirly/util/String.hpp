19d5df106ec71957a7eb3d820a2d5d7c4b635f27
Mark Aylett <mark.aylett@gmail.com>
2016-11-10 21:33:19 +0000

    Add stou16 function.

--- include/swirly/util/String.hpp
+++ include/swirly/util/String.hpp
@@ -307,6 +307,8 @@ void setCString(char (&lhs)[SizeN], std::string_view rhs) noexcept
   setCString(lhs, rhs.data(), rhs.size());
 }
 
+SWIRLY_API uint16_t stou16(std::string_view sv) noexcept;
+
 SWIRLY_API uint32_t stou32(std::string_view sv) noexcept;
 
 SWIRLY_API uint64_t stou64(std::string_view sv) noexcept;
