c98518e63b216854ee84a963dcf07337e5c17389
Mark Aylett <mark.aylett@gmail.com>
2017-01-03 06:43:00 +0000

    Update copyright notices

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 -- The Restful Matching-Engine.
--- Copyright (C) 2013, 2016 Swirly Cloud Limited.
+-- Copyright (C) 2013, 2017 Swirly Cloud Limited.
 --
 -- This program is free software; you can redistribute it and/or modify it under the terms of the
 -- GNU General Public License as published by the Free Software Foundation; either version 2 of the
