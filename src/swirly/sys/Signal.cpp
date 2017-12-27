a09adceb76f204f4cd023d0de46d125ce9048ce9
Mark Aylett <mark.aylett@gmail.com>
2017-12-27 18:53:34 +0000

    Add Signal Component

---
+++ src/swirly/sys/Signal.cpp
@@ -0,0 +1,54 @@
+/*
+ * The Restful Matching-Engine.
+ * Copyright (C) 2013, 2017 Swirly Cloud Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#include "Signal.hpp"
+
+#include <swirly/sys/Error.hpp>
+
+namespace swirly {
+
+SigWait::SigWait()
+{
+    sigemptyset(&newMask_);
+    sigaddset(&newMask_, SIGHUP);
+    sigaddset(&newMask_, SIGINT);
+    sigaddset(&newMask_, SIGUSR1);
+    sigaddset(&newMask_, SIGUSR2);
+    sigaddset(&newMask_, SIGTERM);
+
+    const auto err = pthread_sigmask(SIG_BLOCK, &newMask_, &oldMask_);
+    if (err != 0) {
+        throw std::system_error{sys::makeError(err), "pthread_sigmask"};
+    }
+}
+
+SigWait::~SigWait() noexcept
+{
+    // Restore original signal mask.
+    pthread_sigmask(SIG_SETMASK, &oldMask_, nullptr);
+}
+
+int SigWait::operator()() const
+{
+    int sig;
+    const auto err = sigwait(&newMask_, &sig);
+    if (err != 0) {
+        throw std::system_error{sys::makeError(err), "sigwait"};
+    }
+    return sig;
+}
+
+} // namespace swirly
---
+++ src/swirly/sys/Signal.hpp
@@ -0,0 +1,47 @@
+/*
+ * The Restful Matching-Engine.
+ * Copyright (C) 2013, 2017 Swirly Cloud Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#ifndef SWIRLY_SYS_SIGNAL_HPP
+#define SWIRLY_SYS_SIGNAL_HPP
+
+#include <swirly/Config.h>
+
+#include <signal.h>
+
+namespace swirly {
+
+class SWIRLY_API SigWait {
+  public:
+    SigWait();
+    ~SigWait() noexcept;
+
+    // Copy.
+    SigWait(const SigWait&) = delete;
+    SigWait& operator=(const SigWait&) = delete;
+
+    // Move.
+    SigWait(SigWait&&) = delete;
+    SigWait& operator=(SigWait&&) = delete;
+
+    int operator()() const;
+
+  private:
+    sigset_t newMask_, oldMask_;
+};
+
+} // namespace swirly
+
+#endif // SWIRLY_SYS_SIGNAL_HPP
