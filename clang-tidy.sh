108fa509a45bceb29c75b5e8fe5b7316908d5257
Mark Aylett <mark.aylett@gmail.com>
2018-05-04 15:26:44 +0100

    Additional clang-tidy options

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -2,4 +2,5 @@
 set -e
 export PATH=/usr/share/clang:$PATH
 build=${SWIRLY_BUILD:-$HOME/build/swirly}
-exec run-clang-tidy.py -header-filter='.*' -fix -p=$build
+run-clang-tidy.py -header-filter='.*' -fix -p=$build
+git checkout -f src/swirly/contrib src/swirly/web/RestBody.cpp
