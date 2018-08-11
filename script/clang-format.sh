0d639da2218f0970a30d800d99936755a55a52ca
Mark Aylett <mark.aylett@gmail.com>
2018-08-11 20:59:02 +0100

    Fix clang-format script
    The clang-format.sh script was still referencing the recently removed
    example directory.

--- script/clang-format.sh
+++ script/clang-format.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 format=${CLANG_FORMAT:-clang-format}
-find example src \( ! -name "RestBody.cpp" -a -name "*.[ch]pp" -o -name "*.hxx" \) \
+find src \( ! -name "RestBody.cpp" -a -name "*.[ch]pp" -o -name "*.hxx" \) \
      -exec $format -i --style=file {} \;
