bbb9024cb6da91e00d7a9ebfc72c60f93fe382de
Mark Aylett <mark.aylett@gmail.com>
2018-08-30 06:37:16 +0100

    Buffered echo clnt and serv
    The EchoClnt and EchoServ applications have been extended to
    demonstrate use of the Buffer class along with a line-parser that
    correctly handles line fragments and multiple lines in a single read.

--- src/swirly/util/Tokeniser.hpp
+++ src/swirly/util/Tokeniser.hpp
@@ -18,6 +18,7 @@
 #define SWIRLY_UTIL_TOKENISER_HPP
 
 #include <swirly/util/String.hpp>
+#include <swirly/util/Time.hpp>
 
 #include <algorithm>
 #include <array>
@@ -46,7 +47,18 @@ class Tokeniser {
         i_ = buf_.cbegin();
         j_ = std::find_first_of(i_, buf_.cend(), delims_.cbegin(), delims_.cend());
     }
+    /**
+     * Returns total bytes consumed.
+     */
+    std::size_t consumed() const noexcept { return i_ - buf_.cbegin(); }
+    /**
+     * Returns true if all bytes have been consumed.
+     */
     bool empty() const noexcept { return i_ == buf_.cend(); }
+    /**
+     * Returns true if a delimiter was found in the remaining data.
+     */
+    bool has_delim() const noexcept { return j_ != buf_.cend(); }
     std::string_view top() const noexcept { return buf_.substr(i_ - buf_.cbegin(), j_ - i_); }
     std::string_view next() noexcept
     {
@@ -70,6 +82,17 @@ class Tokeniser {
     // Assumption: std::string_view iterators are equivalent across copies of the std::string_view.
     std::string_view::const_iterator i_, j_;
 };
+
+template <typename FnT>
+std::size_t parseLine(std::string_view buf, Time now, FnT fn)
+{
+    Tokeniser lines{buf, "\n"sv};
+    while (lines.has_delim()) {
+        fn(lines.top(), now);
+        lines.pop();
+    }
+    return lines.consumed();
+}
 
 template <std::size_t N>
 using Row = std::array<std::string_view, N>;
